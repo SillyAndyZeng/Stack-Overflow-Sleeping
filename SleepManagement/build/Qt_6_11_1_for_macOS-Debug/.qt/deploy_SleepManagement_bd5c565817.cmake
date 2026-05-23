1158747e912e23f5b2f337aae01fa6b75fac48a1
wang <18611030858@163.com>
2026-05-23 21:12:46 +0800

    chore: 清除

--- SleepManagement/build/Qt_6_11_1_for_macOS-Debug/.qt/deploy_SleepManagement_bd5c565817.cmake
+++
@@ -1,7 +0,0 @@
-include("/Users/sunny_cy/Desktop/大一下/大一下课程实记/程序设计实习/作业/大作业/程序包/SleepManagement/build/Qt_6_11_1_for_macOS-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/SleepManagement-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "SleepManagement.app"
-)
